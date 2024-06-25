6c6c07d4e500570c22952e9943dd553ecdf6ef03
JIP <kistar99@gmail.com>
2024-06-25 17:24:16 +0900

    플레이어 이동 로직 작업중

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,15 @@
+using Assets.PixelFantasy.Common.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : Creature // 필요한가?
+{
+    [SerializeField] private PlayerAnimation _playerAnimation;
+    [SerializeField] private PlayerMovement _playerMovement;
+
+    private void Awake()
+    {
+
+    }
+}
